75c03ef8ce71dbd7c19033a7ab2c1a922570436d
aky <84511282+tdxf@users.noreply.github.com>
2021-08-01 10:45:22 -0300

    Added a base64 encoder/decoder

--- src/commands/cmd_other.py
+++ src/commands/cmd_other.py
@@ -3,6 +3,8 @@ Various simple commands
 """
 
 
+from base64 import b64encode, b64decode
+
 from .cmds import cmd
 
 
@@ -14,3 +16,13 @@ async def tob(_, arg: str, **_k) -> tuple:
 @cmd()
 async def fromb(_, arg: str, **_k) -> tuple:
     return ''.join([chr(int(num_str, 2)) for num_str in arg.split()]),
+
+
+@cmd()
+async def to64(_, arg: str, **_k) -> tuple:
+    return b64encode(bytes(arg, 'UTF-8')).decode(),
+
+
+@cmd()
+async def from64(_, arg: str, **_k) -> tuple:
+    return b64decode(arg).decode(),
